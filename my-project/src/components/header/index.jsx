e6849cc7c0565712b2ab9996b069ad14d4e02ee8
Jigar Unagar <officialjigarunagar@gmail.com>
2025-05-28 01:13:56 +0530

    update file

---
+++ my-project/src/components/header/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+im
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip py-2">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <div className="coll w-[50%]">
+              <p className="text-[14px] font-[500]">
+                Get up to 50% off new season styles, limited time only
+              </p>
+            </div>
+
+            <div className="col2 flex items-center justify-end ">
+              <ul>
+                <li className="list-none">
+                  
+                </li>
+              </ul>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
