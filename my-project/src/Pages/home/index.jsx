44e98b6971f1c78a932d563bd795236aec9f6f1a
Jigar Unagar <officialjigarunagar@gmail.com>
2025-05-31 19:47:29 +0530

    update

--- my-project/src/Pages/home/index.jsx
+++ my-project/src/Pages/home/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HomeSlider from '../../components/HomeSlider'
+import HomeCartSlider from '../../components/HomeCatSlider'
 
 function Home() {
   return (
     <>
     <HomeSlider/>
+    <HomeCartSlider/>
+
+
+    <br /><br /><br />
+    <br /><br /><br />
     </>
   )
 }
---
+++ my-project/src/components/HomeCatSlider/index.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
+
+function HomeCartSlider() {
+  return (
+    <div className="homeCatSlider py-3">
+      <div className="container">
+        <Swiper
+          slidesPerView={8}
+          navigation={true}
+          spaceBetween={10}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px] transition-all"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-8 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg"
+                  alt=""
+                  className="w-[60px]"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Smart Tablet</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default HomeCartSlider;
