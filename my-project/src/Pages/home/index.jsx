eb5654221769073632cc5065b8415cbaefbd1594
Jigar Unagar <officialjigarunagar@gmail.com>
2025-06-01 00:00:52 +0530

    update

--- my-project/src/Pages/home/index.jsx
+++ my-project/src/Pages/home/index.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import HomeSlider from '../../components/HomeSlider'
-import HomeCartSlider from '../../components/HomeCatSlider'
+import React from "react";
+import HomeSlider from "../../components/HomeSlider";
+import HomeCartSlider from "../../components/HomeCatSlider";
+import { TbTruckDelivery } from "react-icons/tb";
 
 function Home() {
   return (
     <>
-    <HomeSlider/>
-    <HomeCartSlider/>
-    
-    <section className='py-5 bg-white'>
-      <div className="container">
-        <div className="freeShipping w-full p-4 border-[black]" >
-          dsd
-        </div>
-      </div>
-    </section>
+      <HomeSlider />
+      <HomeCartSlider />
 
-    <br /><br />
+      <section className="py-16 bg-white">
+        <div className="container">
+          <div className="freeShipping w-full py-4 p-4 flex items-center justify-between rounded-md">
+            <div className="col1 flex items-center gap-4">
+              <TbTruckDelivery className="text-[50px]" />
+              <span className="text-[20px] font-[600]">Free Shipping</span>
+            </div>
+            <div className="col2">
+              <p className="mb-0 font-[500]">
+                Free Delivery Now On your First Order abd over $200
+              </p>
+            </div>
+            <p className="font-bold text-[25px]">- Only $200*</p>
+          </div>
+        </div>
+      </section>
+
+      <br />
+      <br />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
